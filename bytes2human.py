6757237312d7efcfe9d670df64637be9f7c30035
return40 <leeseoungik@gmail.com>
2018-11-15 01:33:12 +0900

    sample

---
+++ bytes2human.py
@@ -0,0 +1,28 @@
+
+def bytes2human(n):
+    # http://code.activestate.com/recipes/578019
+    # >>> bytes2human(10000)
+    # '9.8K'
+    # >>> bytes2human(100001221)
+    # '95.4M'
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 1 << (i + 1) * 10
+    for s in reversed(symbols):
+        if n >= prefix[s]:
+            value = float(n) / prefix[s]
+            return '%.1f%s' % (value, s)
+    return "%sB" % n
+
+
+print(bytes2human(1024))
+#1.0K
+print(bytes2human(1024*1024))
+#1.0M
+print(bytes2human(1024*1024*1024))
+#1.0G
+print(bytes2human(1024*1024*1024*1024))
+#1.0T
+print(bytes2human(1024*1024*1024*1024*1024))
+#1.0P
